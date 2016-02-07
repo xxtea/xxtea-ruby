0d1b6fec47490ea20d7f6347b12a4a29352871b3
Ma Bingyao <mabingyao@gmail.com>
2016-02-08 02:42:24 +0800

    Fixed ruby_xxtea.c

--- ext/xxtea/ruby_xxtea.c
+++ ext/xxtea/ruby_xxtea.c
@@ -10,7 +10,7 @@
 |                                                          |
 | Code Authors: Chen fei <cf850118@163.com>                |
 |               Ma Bingyao <mabingyao@gmail.com>           |
-| LastModified: Feb 7, 2016                                |
+| LastModified: Feb 8, 2016                                |
 |                                                          |
 \**********************************************************/
 
@@ -18,16 +18,15 @@
 #include <ruby/encoding.h>
 #include "xxtea.h"
 
-VALUE encrypt(VALUE mod, VALUE data, VALUE key) {
+VALUE rb_encrypt(VALUE mod, VALUE data, VALUE key) {
 	unsigned char * result;
 	VALUE retval;
-	size_t data_len, key_len, out_len;
+	size_t data_len, out_len;
 
 	Check_Type(data, T_STRING);
 	Check_Type(key, T_STRING);
 
 	data_len = RSTRING_LEN(data);
-	key_len = RSTRING_LEN(key);
 
 	result = xxtea_encrypt(RSTRING_PTR(data), data_len, RSTRING_PTR(key), &out_len);
 
@@ -38,16 +37,15 @@ VALUE encrypt(VALUE mod, VALUE data, VALUE key) {
 	return retval;
 }
 
-VALUE decrypt(VALUE mod, VALUE data, VALUE key) {
+VALUE rb_decrypt(VALUE mod, VALUE data, VALUE key) {
 	unsigned char *result;
 	VALUE retval;
-	size_t data_len, key_len, out_len;
+	size_t data_len, out_len;
 
 	Check_Type(data, T_STRING);
 	Check_Type(key, T_STRING);
 
 	data_len = RSTRING_LEN(data);
-	key_len = RSTRING_LEN(key);
 
 	result = xxtea_decrypt(RSTRING_PTR(data), data_len, RSTRING_PTR(key), &out_len);
 
@@ -58,13 +56,13 @@ VALUE decrypt(VALUE mod, VALUE data, VALUE key) {
 	return retval;
 }
 
-VALUE decrypt_utf8(VALUE mod, VALUE data, VALUE key) {
-	return rb_enc_associate(decrypt(mod, data, key), rb_utf8_encoding());
+VALUE rb_decrypt_utf8(VALUE mod, VALUE data, VALUE key) {
+	return rb_enc_associate(rb_decrypt(mod, data, key), rb_utf8_encoding());
 }
 
 void Init_xxtea() {
     VALUE XXTEA = rb_define_module("XXTEA");
-    rb_define_singleton_method(XXTEA, "encrypt", encrypt, 2);
-    rb_define_singleton_method(XXTEA, "decrypt", decrypt, 2);
-	rb_define_singleton_method(XXTEA, "decrypt_utf8", decrypt_utf8, 2);
+    rb_define_singleton_method(XXTEA, "encrypt", rb_encrypt, 2);
+    rb_define_singleton_method(XXTEA, "decrypt", rb_decrypt, 2);
+	rb_define_singleton_method(XXTEA, "decrypt_utf8", rb_decrypt_utf8, 2);
 }

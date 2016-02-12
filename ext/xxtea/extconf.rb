97b5202458e90b81b995757eb0496fa11f83abcb
Ma Bingyao <mabingyao@gmail.com>
2016-02-12 12:24:31 +0800

    Fixed extconf.rb

--- ext/xxtea/extconf.rb
+++ ext/xxtea/extconf.rb
@@ -9,7 +9,7 @@ else
     file.puts 'set(CMAKE_MACOSX_RPATH 1)' if RUBY_PLATFORM=~/darwin/
     file.puts 'set(LIBXXTEA_SRC ' + File.expand_path(File.dirname(__FILE__) + '/xxtea.c') +')'
     file.puts 'add_library(xxtea SHARED ${LIBXXTEA_SRC})'
-    file.puts 'install(TARGETS xxtea DESTINATION ' + File.expand_path(File.dirname(__FILE__) + '/../../lib/xxtea') + ')'
+    file.puts 'install(TARGETS xxtea LIBRARY DESTINATION ' + File.expand_path(File.dirname(__FILE__) + '/../../lib/xxtea') + ')'
   end
   system('cmake -G "Unix Makefiles" .')
 end
